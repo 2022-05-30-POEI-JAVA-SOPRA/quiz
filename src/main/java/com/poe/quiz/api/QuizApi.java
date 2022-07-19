cf59338f48e771f61850daf9c07882d7fee52e74
jcdominguez <jcd.pro@gmail.com>
2022-07-19 10:22:51 +0200

    Creation nouvelle route dans l'API web pour permettre à un joueur de valider sa réponse

--- src/main/java/com/poe/quiz/api/QuizApi.java
+++ src/main/java/com/poe/quiz/api/QuizApi.java
@@ -3,10 +3,14 @@ package com.poe.quiz.api;
 import com.poe.quiz.business.Game;
 import com.poe.quiz.business.Question;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 
 @Path("questions")
 public class QuizApi {
@@ -19,4 +23,17 @@ public class QuizApi {
         Game game = new Game();
         return game.getStore().getQuestions();
     }
+    
+    @Path("{questionId}/play/{answerId}")
+    @POST()
+    @Produces({ MediaType.APPLICATION_JSON })
+    public ValidationDTO validate(@PathParam("questionId") Long questionId, 
+                                    @PathParam("answerId") Long answerId) {
+        Game game = new Game();
+        boolean win = game.validateAnswer(questionId, answerId);
+        ValidationDTO response = new ValidationDTO();
+        response.setWin(win);
+        return response;
+    }
+    
 }
